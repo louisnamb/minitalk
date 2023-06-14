ab89ba11f4d9d6f881e7dc67a378784038520003
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-06-14 15:33:16 +0930

    Add files via upload

--- client.c
+++ client.c
@@ -8,30 +8,21 @@
 
 /*
 gcc client.c libft/ft_atoi.c libft/ft_memset.c printf/ft_printf.c printf/ft_strlen.c printf/hexchars.c printf/putchars.c -o client
-
-Client: Takes PID and string, converting each char to binary
-and sending that to the server.
-
-✅ Server prints PID
-
-✅ Client uses PID and string, sending bits
-to represent the characters. 
-
-✅ Server recieves it and prints it.
 */
 
 int main(int argc, char **argv)
 {
-	/* Get p1's process ID. */
+	int		i;
+	int		shift;
+	int		stop;
+	pid_t	pid;
+
+	pid = ft_atoi(argv[1]);
+	i = 0;
+	shift = 7;
+	stop = 0;
 	if (argc != 3)
 		return (1);
-	pid_t pid = ft_atoi(argv[1]);
-	
-	/* Signal setup */
-	int i = 0;
-	int shift = 7;
-	int stop = 0;
-	/* Check if the PID is valid */
 	if (kill(pid, 0) == -1)
 	{
 		perror("Invalid PID");
@@ -42,24 +33,21 @@ int main(int argc, char **argv)
 		shift = 7;
 		while (shift >= 0)
 		{
-			printf("%d\n", (argv[2][i] >> shift) & 1);
 			if (((argv[2][i] >> shift) & 1) == 1)
-				kill(pid, SIGUSR2); //31
+				kill(pid, SIGUSR2);
 	
 			else
-				kill(pid, SIGUSR1); //30
-			usleep(1000);
+				kill(pid, SIGUSR1);
+			usleep(50);
 			shift--;
 		}
-		printf("\n");
 		i++;
 	}
 	while (stop < 8)
 	{
 		kill(pid, SIGUSR1);
-		usleep(1000);
+		usleep(50);
 		stop++;
 	}
-	/* Send the string passed to the server */
 	return 0;
 }
--- server.c
+++ server.c
@@ -8,27 +8,14 @@
 
 /*
 gcc server.c libft/ft_atoi.c libft/ft_memset.c printf/ft_printf.c printf/ft_strlen.c printf/hexchars.c printf/putchars.c -o server
-
-Server: prints PID and recieves bits, converting them to char,
-
-Server prints PID
-
-Client recieves PID and string, sending bits
-to represent the characters. 
-
-Server recieves it and prints it.
 */
-
-struct bytes {
-	int data[8];
-	struct bytes *next;
-};
 
 static void bit2char(int byte[])
 {
-	int i;
-	char sum;
-    int placeval;
+	int		i;
+	char	sum;
+    int		placeval;
+
 	sum = 0;
 	i = 7;
     placeval = 1;
@@ -46,7 +33,7 @@ static void bit2char(int byte[])
 
 int end(int byte[])
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (byte[i] == 0)		
@@ -54,66 +41,37 @@ int end(int byte[])
 	return (i == 8);	
 }
 
-struct bytes	*ft_free(struct bytes *data, struct bytes *new)
-{
-	free(data);
-	return (new);
-}
-
 static void get_bit(int signals)
 {
-	struct bytes *num;
-	static int index = 0;
-	static int arr = 0;
-	int j = 0;
-	num = NULL;
-	if (num == NULL)
-	{
-		num = malloc(sizeof(num));
-		num->next = NULL;
-	}
-	num->data[index] = (signals == 31);
-	ft_printf("%d", (signals == 31));
+	int			num[8];
+	static int	index;
+
+	if (index == 8)
+		index = 0;
+	num[index] = (signals == 31);
 	index++;
 	if (index == 8)
 	{
-		ft_printf("\n");
-		index = 0;
-		if (!end(num->data))
-		{
-			printf("new node\n");
-			num = ft_free(num, malloc(sizeof(num)));
-		}
-		else
-		{
-			bit2char(num->data);
-			ft_free(num, NULL);
+		bit2char(num);
+		if (end(num))
 			return ;
-		}
-		bit2char(num->data);
-		arr++;
 	}
 }
 
 int main()
 {
-    pid_t pid;
+    pid_t	pid;
+	struct sigaction sa;
 
     pid = getpid();
     ft_printf("%d\n", pid);	
-	/* Signal setup */
-	struct sigaction sa;
 	ft_memset(&sa, 0, sizeof(sa));
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
 	sa.sa_handler = get_bit;
 	sigaction(SIGUSR1, &sa, 0);
 	sigaction(SIGUSR2, &sa, 0);
-	// while loop that checks for signals
 	while (1)
 		pause();
 	return (0);
 }
-/*
-Infinite while loop is happening.
-*/
