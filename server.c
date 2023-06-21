1db5d9052f51ee25887a7b49b2cee7c46b659621
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-06-21 14:35:35 +0930

    Add files via upload

--- server.c
+++ server.c
@@ -25,14 +25,8 @@ static char	bit2char(int byte[])
 
 	sum = 0;
 	i = 8;
-//	printf("\033[0;33mbit2char\033[0m\n");
-//	printf("bit2char\n");
 	while (i-- > 0)
-	{
 		sum |= (byte[i] << (7 - i));
-	//	printf("\033[0;33m%d\033[0m", byte[i]);
-	}
-//	printf("\n");
 	return (sum);
 }
 
@@ -43,43 +37,39 @@ int	end(int byte[], char **string, int *pos, int *index)
 	i = 0;
 	while (byte[i] == 0)
 		i++;
-//	printf("\033[0;31mend: i check: %d\n\033[0m\n", 1);
 	if (i == 8)
 	{
-//		printf("about to print\n");
-		write(1, *(string), *pos);
-		free(*(string));
-		*(string) = NULL;
+	//	printf("\033[0;31mpos: %d index: %d str: %d\033[0m\n", *pos, *index, (*(string) == NULL));
+		(*string)[(*pos)] = '\0';
+		write(1, *string, (*pos + 1));
+		free(*string);
+		*string = NULL;
 		*index = 0;
 		*pos = 0;
+	//	printf("\033[0;31mpos: %d index: %d str: %d\033[0m\n", *pos, *index, (*(string) == NULL));
 	}
 	return (i == 8);
 }
 
-char	*ft_realloc(char **str, long *size, size_t pos)
+char	*ft_realloc(char **str, long *size, int pos)
 {
 	size_t	i;
 	char	*new_str;
 
 	i = 0;
-	//printf("\033[0;32minside ft_realloc\033[0m\n");
 	if ((*size) == pos)
 		*size += 50;
 	new_str = malloc(sizeof(char) * ((*size) + 1));
 	if (!new_str)
 		return (NULL);
-//	printf("success allocation\n");
 	while (i < pos && (*str)[i] && *size != 0)
 	{
-//		printf("inside while reallic\n");
 		new_str[i] = (*str)[i];
 		i++;
 	}
-//	printf("siz:%ld insert null\n", *size);
-	new_str[(*size) - 1] = '\0';
-	if (i == pos || (*str))
+	new_str[(*size)] = '\0';
+	if (i == pos && (*str))
 	{
-	//	printf("\033[0;32mfreeing\033[0m\n");
 		free(*str);
 		*str = NULL;
 	}
@@ -94,19 +84,23 @@ static void	get_bit(int signals)
 	static char	*string;
 	static int	pos;
 
-	if (pos == max)// && end(num, &string, &pos, &index))
+	if (max == 0 || pos == max)
 	{
+		printf("\033[0;31mpos: %d index: %ld str: %d\033[0m\n", pos, max, ((string) == NULL));
 		string = ft_realloc(&string, &max, pos);
 		max += 50;
+		printf("\033[0;31mpos: %d index: %ld str: %d\033[0m\n", pos, max, ((string) == NULL));
 	}
 	num[index++] = (signals == 31);
-	if (index == 8)
+	if (index == 8 && pos < max)
 	{
 		string[pos++] = bit2char(num);
+	//	printf("\033[0;34mpos: %d\033[0m\n", pos);
 		index = 0;
 		if (end(num, &string, &pos, &index))
 		{
 			max = 0;
+		//	printf("\033[0;31mmax: %ld pos: %d index: %d str: %d\033[0m\n", max, pos, index, ((string) == NULL));
 			return ;
 		}
 	}
