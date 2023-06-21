b45119413da62deb6779ad64983e521ea5725bc4
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-06-21 16:18:05 +0930

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,59 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/06/21 15:46:52 by lnambaji          #+#    #+#              #
+#    Updated: 2023/06/21 16:16:57 by lnambaji         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+CNAME			=	client
+
+SNAME			=	server
+
+SRCS_SERVER		=	server.c libft/ft_atoi.c libft/ft_memset.c \
+            	printf/ft_printf.c printf/ft_strlen.c \
+				printf/hexchars.c printf/putchars.c
+
+SRCS_CLIENT		=	client.c libft/ft_atoi.c libft/ft_memset.c \
+				printf/ft_printf.c printf/ft_strlen.c printf/hexchars.c \ 
+				printf/putchars.c
+
+OBJS_SERVER = $(SRCS_SERVER:.c=.o)
+OBJS_CLIENT = $(SRCS_CLIENT:.c=.o)
+
+LIB_SRCS		=	libft/ft_atoi.c libft/ft_memset.c
+
+PRINT_SRCS		=	printf/ft_printf.c printf/ft_strlen.c printf/hexchars.c \
+				printf/putchars.c
+OBJS		=	$(SRCS:.c=.o) $(LIB_SRCS:.c=.o) $(PRINT_SRCS:.c=.o) 
+
+CFLAGS		=	-Wall -Wextra -Werror
+
+RM			=	rm -f
+
+CC			=	gcc
+
+all: $(CNAME) $(SNAME)
+
+%.o: %.c
+	$(CC) -g $(FLAGS) -c $< -o $@
+
+$(CNAME): $(OBJS_CLIENT)
+    $(CC) $(CFLAGS) -o $(CNAME) $(OBJS_CLIENT)
+
+$(SNAME): $(OBJS_SERVER)
+    $(CC) $(CFLAGS) -o $(SNAME) $(OBJS_SERVER)
+
+clean:
+	$(RM) $(OBJS)
+
+fclean: clean
+	$(RM) $(CNAME) $(SNAME)
+
+re: fclean all
+
+.PHONY:	all clean fclean re
--- minitalk.h
+++ minitalk.h
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   minitalk.h                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/15 15:40:12 by lnambaji          #+#    #+#             */
-/*   Updated: 2023/06/15 15:40:17 by lnambaji         ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   minitalk.h										 :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: lnambaji <lnambaji@student.42.fr>		  +#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2023/06/15 15:40:12 by lnambaji		  #+#	#+#			 */
+/*   Updated: 2023/06/21 15:50:27 by lnambaji		 ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include "printf/ft_printf.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "printf/ft_printf.h"
+# include <sys/types.h>
+# include <signal.h>
 
-struct sigaction {
-    void       (*sa_handler)(int);
-    sigset_t   sa_mask;
-    int        sa_flags;
-    void       (*sa_sigaction)(int, siginfo_t *, void *);
-};
+static char	bit2char(int byte[]);
+int end(int byte[], char **string, int *pos, int *index)
+char *ft_realloc(char **str, long *size, int pos)
+static void get_bit(int signals)
 
 #endif
